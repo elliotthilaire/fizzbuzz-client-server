e6bb59945266b4d15b93708485e3eafd47c0e830
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-07-15 14:25:27 +0100

    Add basic fizzbuzz functionality

---
+++ fizzbuzz/lib/fizzbuzzer.rb
@@ -0,0 +1,11 @@
+class FizzBuzzer
+  def self.call(arg)
+    number = Integer(arg)
+
+    string = ''
+    string << 'Fizz' if (number % 3).zero?
+    string << 'Buzz' if (number % 5).zero?
+
+    string.empty? ? number : string
+  end
+end
--- fizzbuzz/spec/fizzbuzzer_spec.rb
+++ fizzbuzz/spec/fizzbuzzer_spec.rb
@@ -3,21 +3,29 @@ require_relative "../lib/fizzbuzzer"
 RSpec.describe FizzBuzzer, "#call" do
   context "with an number divisible by 3" do
     it "returns 'Fizz'" do
+      fizzbuzzed = FizzBuzzer.(3)
+      expect(fizzbuzzed).to eq 'Fizz'
     end
   end
 
   context "with a number divisible by 5" do
     it "returns 'Buzz'" do
+      fizzbuzzed = FizzBuzzer.(5)
+      expect(fizzbuzzed).to eq 'Buzz'
     end
   end
 
   context "with a number divisible by 3 and 5" do
     it "returns 'FizzBuzz'" do
+      fizzbuzzed = FizzBuzzer.(3 * 5)
+      expect(fizzbuzzed).to eq 'FizzBuzz'
     end
   end
 
   context "with a number not divisble by 3 or 5" do
     it "returns the number" do
+      fizzbuzzed = FizzBuzzer.(1)
+      expect(fizzbuzzed).to eq 1
     end
   end
 
