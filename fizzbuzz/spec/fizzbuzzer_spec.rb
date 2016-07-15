e3061ee066fb921c713d0af5d96547bb4be18c67
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-07-15 14:07:11 +0100

    Add some empty tests

---
+++ fizzbuzz/spec/fizzbuzzer_spec.rb
@@ -0,0 +1,37 @@
+require_relative "../lib/fizzbuzzer"
+
+RSpec.describe FizzBuzzer, "#call" do
+  context "with an number divisible by 3" do
+    it "returns 'Fizz'" do
+    end
+  end
+
+  context "with a number divisible by 5" do
+    it "returns 'Buzz'" do
+    end
+  end
+
+  context "with a number divisible by 3 and 5" do
+    it "returns 'FizzBuzz'" do
+    end
+  end
+
+  context "with a number not divisble by 3 or 5" do
+    it "returns the number" do
+    end
+  end
+
+  context "with an argument that can be coerced to an Integer" do
+    it "works" do
+    end
+  end
+
+  context "with an argument that cannot be coerced to a Integer" do
+    it "raises an ArguementError" do
+    end
+  end
+
+  context "with a negative number" do
+  end
+
+end
