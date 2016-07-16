f97affe6fd866daf76cec657885e763e2d86517d
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-07-16 13:21:04 +0100

    Add empty Page specs

--- fizzbuzz/spec/page_spec.rb
+++ fizzbuzz/spec/page_spec.rb
@@ -1,7 +1,25 @@
 require_relative "../lib/page"
 
-RSpec.describe Page, "#" do
+RSpec.describe Page, "#new" do
 
+  it 'fetches pages of items using supplied fetcher' do
+  end
 
+  it 'returns array with number or per page items' do
+  end
+
+  it 'is enumerable' do
+  end
+
+  context 'with default arguments' do
+    it 'defaults to first page' do
+    end
+
+    it 'starts first item at 1' do
+    end
+
+    it 'defaults per_page to 100' do
+    end
+  end
 
 end
