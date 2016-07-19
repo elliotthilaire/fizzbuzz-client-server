1fed3f1c5e00388add63406e7eb53a5d35ba260b
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-07-19 09:30:59 +0100

    Updated favourites_controller spec

--- rails-server/spec/controllers/favourites_controller_spec.rb
+++ rails-server/spec/controllers/favourites_controller_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 RSpec.describe FavouritesController, type: :controller do
 
   describe "POST #create" do
-    context "when favourite already exists" do
+    context "when favourite does not exist" do
+      it 'creates a favourite' do
+        expect { post :create, favourite: {number: 1} }.to change(Favourite, :count).by(1)
+      end
     end
 
-    context "when favourite does not exist" do
+    context "when favourite already exists" do
     end
 
     context "when given an invalid number" do
@@ -15,6 +18,10 @@ RSpec.describe FavouritesController, type: :controller do
 
   describe "DELETE #destroy" do
     context "when favourite exists" do
+      it 'deletes the favourite' do
+        Favourite.create(number: 1)
+        expect { delete :destroy, number: 1 }.to change(Favourite, :count).by(-1)
+      end
     end
 
     context "when favourite does not exist" do
