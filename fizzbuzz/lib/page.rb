163eb3453c1eaea3ed3d50eb254b9137b67e7df9
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-07-20 12:37:34 +0100

    Refactor to partials

--- fizzbuzz/lib/page.rb
+++ fizzbuzz/lib/page.rb
@@ -1,10 +1,10 @@
 class Page
   include Enumerable
 
-  attr_reader :page, :per_page
+  attr_reader :page_number, :per_page
 
-  def initialize(page: 1, per_page: 100, starts_at: 1, &block)
-    @page = Integer(page)
+  def initialize(page_number: 1, per_page: 100, starts_at: 1, &block)
+    @page_number = Integer(page_number)
     @per_page = Integer(per_page)
     @starts_at = Integer(starts_at)
 
@@ -22,10 +22,10 @@ private
   end
 
   def start_range
-    @starts_at + (@page * @per_page) - @per_page
+    @starts_at + (@page_number * @per_page) - @per_page
   end
 
   def end_range
-    @starts_at + (@page * @per_page) - 1
+    @starts_at + (@page_number * @per_page) - 1
   end
 end
--- rails-server/app/controllers/fizzbuzz_controller.rb
+++ rails-server/app/controllers/fizzbuzz_controller.rb
@@ -4,7 +4,7 @@ class FizzbuzzController < ApplicationController
     page = params[:page] || 1
     per_page = params[:per_page] || 100
 
-    @page_of_fizzbuzzes = Page.new(page: page, per_page: per_page) do |item_number|
+    @page_of_fizzbuzzes = Page.new(page_number: page, per_page: per_page) do |item_number|
       Fizzbuzz.new(item_number, favourites: favourites)
     end
   end
