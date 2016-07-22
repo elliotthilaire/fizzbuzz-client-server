b9a5e9d684c5bae1019e3edf44626dc122ac5718
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-07-22 15:14:15 +0100

    Add fizzbuzz_favourite_delete starter code

---
+++ go-client/src/github.com/elliotthilaire/fizzbuzz_favourite_delete/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+  "net/url"
+)
+
+func main() {
+  number := "1"
+  delete_favourite(number)
+}
+
+func delete_favourite(number string) {
+
+  safeNumber := url.QueryEscape(number)
+
+  url := fmt.Sprintf("http://localhost:3000/api/v1/favourites/%s", safeNumber)
+
+  request, err := http.NewRequest("DELETE", url, nil)
+  if err != nil {
+    log.Fatal("NewRequest: ", err)
+    return
+  }
+
+  // For control over HTTP client headers,
+  // redirect policy, and other settings,
+  // create a Client
+  // A Client is an HTTP client
+  client := &http.Client{}
+
+  // Send the request via a client
+  // Do sends an HTTP request and
+  // returns an HTTP response
+  response, err := client.Do(request)
+  if err != nil {
+    log.Fatal("Do: ", err)
+    return
+  }
+
+  // Callers should close resp.Body
+  // when done reading from it
+  // Defer the closing of the body
+  defer response.Body.Close()
+
+  fmt.Println(response.Status)
+}
+
+
